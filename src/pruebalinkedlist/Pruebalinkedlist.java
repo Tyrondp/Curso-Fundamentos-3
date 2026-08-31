6dce37f3a46ac72cc8ab5e9207f02e157d38154e
Tyrondp <aarongalaz769@gmail.com>
2026-08-31 12:15:48 -0700

    commit 4

--- src/pruebalinkedlist/Pruebalinkedlist.java
+++ src/pruebalinkedlist/Pruebalinkedlist.java
@@ -1,15 +1,18 @@
 package pruebalinkedlist;
+
 /*
-@author Aaron Galaz 
+@author Aaron Galaz
 @date 17-08-2026
-*/
+ */
 
 class Student {
+
     String name;
     int score;
+    int index;
     Student next;
 
-    public Student(String name, int score){
+    public Student(String name, int score) {
         this.name = name;
         this.score = score;
         this.next = null;
@@ -17,69 +20,103 @@ class Student {
 }
 
 class LnkdLst {
-    Student head;
 
-    public LnkdLst(){ // Constructor
-        this.head = null;
+    Student head;
+    Student tail;
+    int index = 0;
+    
+    public LnkdLst() // Constructor
+    { 
+        this.head = null; 
+        this.tail = null;
     }
 
     // Agrega un nuevo estudiante al final de la lista
-    public void add(String name, int score){
+    public void add(String name, int score) {
         Student newStudent = new Student(name, score);
-
         if (head == null) {
-            head = newStudent;
+            head = newStudent; //si head es nulo, agregamos nuevo estudiante (newStudent)
+            tail = newStudent;
+            this.index++;
             return;
         }
-
-        Student current = head;
-
-        while (current.next != null){
-            current = current.next;
-        }
-
-        current.next = newStudent;
+        tail.next = newStudent;
+        tail = newStudent;
+        this.index++;
     }
 
     public void print() {
         Student current = this.head;
-
-        while (current != null){
+        while (current != null) {
             System.out.println(current.name + " - " + current.score);
             current = current.next;
         }
     }
 
-    public Student retrieve(String searchName){
+    public Student retrieve(String searchedName) {
         Student current = this.head;
-
-        while (current != null){
-            if (current.name.equals(searchName)){
+        while (current != null) {
+            if (current.name.equals(searchedName)) {
                 return current;
             }
-
             current = current.next;
         }
-
         return null;
+    }
+
+    public void remove(int n) {
+        if (head == null) {return; // Lista vacia
+        }
+        if (n < 0) {return; // Index negativo
+        }
+        if (n >= this.index){return; // Index out of boundsa
+        }
+        if (n == 0) {
+            head = head.next;
+            this.index--;
+            return;
+        }
+        if (n == this.index - 1){
+            Student current = head;
+            while (current.next != tail){
+                current = current.next;
+            }
+            current.next = null;
+            tail = current;
+            this.index--;
+            return;
+            
+        }
+
+        int index = 0;
+        Student current = head;
+        while (current != null && index < (n - 1)) {
+            current = current.next;
+            index++;
+        }
+        if (current == null || current.next == null) {return; // position out of range
+        }
+        current.next = current.next.next;
+        this.index--;
     }
 }
 
 public class Pruebalinkedlist {
-
     public static void main(String[] args) {
         LnkdLst lista = new LnkdLst();
-
-        lista.add("Luis", 90);
-        lista.add("Ada", 95);
-        lista.add("Paul", 78);
+        lista.add("Linus",90);
+        lista.add("Ada",95);
+        lista.add("Paul",78);
         lista.print();
-        Student found = lista.retrieve("Ada");
-        System.out.println("Found: "+found.name+" score:"+found.score);
-        Student notFound = lista.retrieve("Gus");
-        if(notFound!=null){
-                    System.out.println("Found: "+notFound.name+" score:"+notFound.score);
-
+        Student s = lista.retrieve("Ada");
+        if (s != null) {
+            System.out.println("Found: " + s.name + " - " + s.score);
+        } else {
+            System.out.println("Student not found");
         }
+        lista.remove(2);
+        lista.remove(3);
+        System.out.println("After removing the second student:");
+        lista.print();
     }
 }
